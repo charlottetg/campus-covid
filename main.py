a65f8b1ee82c8a6fb32e29b3f017637e29e6e576
Anna Spiro <annaspiro@annas-mbp-9.middlebury.edu>
2020-10-19 12:46:55 -0400

    added constants and a random sampling for before_symptomatic

--- main.py
+++ main.py
@@ -3,19 +3,36 @@ import matplotlib.pyplot as plt
 import scipy
 import random
 from random import choice
+import numpy as np
+
+
+# people constants
+MEAN_SYMPTOMATIC = 6 # 6 days on average
+STANDARD_DEV_SYMPTOMATIC = 1.2 # can change this later
+
+# graph constants
+NUM_STUDENTS = 20
+NUM_CLOSE = 1
+PROB_CLOSE = 2 # will eventually want to make this a probability
+NUM_TANG = 5
+PROB_TANG = 1
+
 
 class Person:
+
     def __init__(self, contacts):
         self.state = "healthy"
         self.contacts = contacts
+
+        # how many days before a person is symptomatic: initalize as -1
         self.before_symptomatic = -1
 
     def get_covid(self):
         # change state to infected asymptomatic
         self.state = "asymptomatic"
 
-        # randomly pick int = # of time steps before symptomatic
-        self.before_symptomatic = 5
+        # randomly sample from disrubtion to get # time steps before symptomatic
+        self.before_symptomatic = np.random.normal(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)
 
     def get_tested(self):
         if (self.state == "asymptomatic"):
@@ -23,15 +40,16 @@ class Person:
 
 class Graph:
     def __init__(self):
-        students = 20
-        close = [1, 2]
-        tangential = [5, 1]
+        close_data = [NUM_CLOSE, PROB_CLOSE]
+        tang_data = [NUM_TANG, PROB_TANG]
+
         # initialize graph
         g_basic = nx.Graph()
+
         # initialize node infos: dumb! slow!
         people = {}
 
-        for i in range(1, students+1): #for all of the students
+        for i in range(1, NUM_STUDENTS+1): #for all of the students
             #if they are not already in the graph?
             """
             if i in people:
@@ -42,29 +60,30 @@ class Graph:
 
             # add contact edges
             # tangential contacts
-            t = random.sample(range(1, students+1), tangential[0])
+            tang_contacts = random.sample(range(1, NUM_STUDENTS+1), tang_data[0])
 
             # close contacts
-            c = random.sample(range(1, students+1), close[0])
+            close_contacts = random.sample(range(1, NUM_STUDENTS+1), close_data[0])
             #for each of their close contacts, we gotta do something here!
 
-            contacts = [(tangential[1], t), (close[1], c)]
+            contacts = [(tang_data[1], tang_contacts), (close_data[1], close_contacts)]
             people[i] = Person(contacts)
 
 
-
         # add edges
-        for i in range(1, students+1):
+        for i in range(1, NUM_STUDENTS+1):
             for pair in people[i].contacts:
-                #print(pair)
+                # print(pair)
                 risk = pair[0]
                 contacts = pair[1]
                 for contact in contacts:
                     g_basic.add_edge(i, contact, weight = risk)
 
 
+        # code for testing
+
         #testing the viewing by giving 5 random people covid
-        for i in random.sample(range(1, students+1), 5):
+        for i in random.sample(range(1, NUM_STUDENTS+1), 5):
             people[i].get_covid()
 
         closecontacts = [(u, v) for (u, v, d) in g_basic.edges(data=True) if d["weight"] == 2 ]
