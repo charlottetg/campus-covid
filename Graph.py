232a8a20f1c02857d4a9b1cf4601a6a32c3ed14e
charlotte gray <charlottetgray@gmail.com>
2020-11-09 13:23:17 -0500

    manually integrated changes from anna-working

--- Graph.py
+++ Graph.py
@@ -2,26 +2,23 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import scipy
 import random
+from Person import Person
 from random import choice
 import numpy as np
 import math as math
 
 class Graph:
 
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
+    def __init__(self, ids_dict):
+        self.ids_dict = ids_dict
         self.day = 0
 
 
     # display graph using networkx
-    def show_graph(self):
-
-        # eventually take these out
-        PROB_CLOSE = .174
-        PROB_TANG = .031
+    def show_graph(self, prob_close, prob_tang):
 
         g = nx.Graph()
-        people_map = self.adjacency_list
+        people_map = self.ids_dict
         people_ids = people_map.keys()
 
         # nodes
@@ -32,8 +29,9 @@ class Graph:
 
         # edges
         for person_id in people_ids:
-            for contact in people_map[person_id].contacts:
-                g.add_edge(person_id, contact[0], weight = contact[1])
+            current_contacts = people_map[person_id].contacts
+            for contact in current_contacts:
+                g.add_edge(person_id, contact, weight = current_contacts[contact])
 
         healthy = [person_id for person_id in g.nodes() if people_map[person_id].state == -1]
         asymptomatic = [person_id for person_id in g.nodes() if people_map[person_id].state >= 0]
@@ -52,8 +50,8 @@ class Graph:
         nx.draw_networkx_labels(g, pos, labels, font_size = 16)
 
         # make edges (something up here)
-        close_contacts = [(u, v) for (u, v, d) in g.edges(data=True) if d["weight"] == PROB_CLOSE]
-        tang_contacts = [(u, v) for (u, v, d) in g.edges(data=True) if d["weight"] == PROB_TANG]
+        close_contacts = [(u, v) for (u, v, d) in g.edges(data=True) if d["weight"] == prob_close]
+        tang_contacts = [(u, v) for (u, v, d) in g.edges(data=True) if d["weight"] == prob_tang]
         dangerous_contacts = [(u, v) for (u, v, d) in g.edges(data=True) if people_map[u].state >= 0 or people_map[v].state >= 0]
 
         # draw edges
@@ -64,53 +62,55 @@ class Graph:
         plt.show()
 
 
+
     # to be called on self and an asymptomatic person: probabalistically gives covid to their contacts
-    def individual_spread(self, spreader_id):
-        for contact in self.adjacency_list[spreader_id].contacts:
-            if self.adjacency_list[contact[0]].state == -1: # healthy, susceptible
-                transmission_prob = contact[1]
+    def individual_spread(self, spreader_id, mean_symptomatic, standard_dev_symptomatic):
+        contacts = self.ids_dict[spreader_id].contacts
+        for person_id in contacts:
+            if self.ids_dict[person_id].state == -1: # healthy, susceptible
+                transmission_prob = contacts[person_id]
 
                 # if edge is traveled
                 if random.choices([True, False], weights = [transmission_prob, (1 - transmission_prob)])[0]: # returns a list with one element
-                    self.adjacency_list[contact[0]].get_covid()
-                    print(str(contact[0]) + " got covid from " + str(spreader_id))
+                    self.ids_dict[person_id].get_covid(mean_symptomatic, standard_dev_symptomatic)
+                    print(str(person_id) + " got covid from " + str(spreader_id))
 
 
     # what happens to the graph every time step
-    def graph_spread(self):
+    def graph_spread(self, mean_symptomatic, standard_dev_symptomatic):
 
         print("day " + str(self.day))
         # make a list of people ids who start off asymptomatic
         current_spreaders = []
-        for person_id in self.adjacency_list.keys():
-            if self.adjacency_list[person_id].state >= 0: # asymptomatic
-                self.adjacency_list[person_id].state -= 1 # decrease by 1
+        for person_id in self.ids_dict:
+            if self.ids_dict[person_id].state >= 0: # asymptomatic
+                self.ids_dict[person_id].state -= 1 # decrease by 1
                 current_spreaders.append(person_id)
 
-            if self.adjacency_list[person_id].state == 0: # now showing symptoms
-                self.adjacency_list[person_id].state = -2 # quarantine (still spread this time step, but not after)
+            if self.ids_dict[person_id].state == 0: # now showing symptoms
+                self.ids_dict[person_id].state = -2 # quarantine (still spread this time step, but not after)
                 print(str(person_id) + " showed symptoms and quarantined")
 
         for spreader in current_spreaders:
-            self.individual_spread(spreader)
+            self.individual_spread(spreader, mean_symptomatic, standard_dev_symptomatic)
 
         # keep track of day
         self.day += 1
 
     # for testing
     def print_contacts_info(self):
-        people_map = self.adjacency_list
+        people_map = self.ids_dict
         people_ids = people_map.keys()
 
-        for person_id in self.adjacency_list.keys():
-            print("person " + str(person_id) + " :" + str(self.adjacency_list[person_id].contacts))
+        for person_id in self.ids_dict.keys():
+            print("person " + str(person_id) + " :" + str(self.ids_dict[person_id].contacts))
 
 
     # also for testing (redundant with next functions)
     def print_stats(self):
-        healthy = [person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state == -1]
-        asymptomatic = [person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state >= 0]
-        quarantined = [person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state == -2]
+        healthy = [person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state == -1]
+        asymptomatic = [person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state >= 0]
+        quarantined = [person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state == -2]
 
         print("day " + str(self.day))
         print("healthy: " + str(len(healthy)))
@@ -120,20 +120,72 @@ class Graph:
 
 
     def num_healthy(self):
-        return len([person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state == -1])
+        return len([person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state == -1])
 
     def num_asymptomatic(self):
-        return len([person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state >= 0])
+        return len([person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state >= 0])
 
     def num_quarantined(self):
-        return len([person_id for person_id in self.adjacency_list.keys() if self.adjacency_list[person_id].state == -2])
+        return len([person_id for person_id in self.ids_dict.keys() if self.ids_dict[person_id].state == -2])
 
 
     # dynamic test a list of people_ids
     def dynamic_test(self, ids_to_test):
         for id in ids_to_test:
-            person = self.adjacency_list[id]
+            person = self.ids_dict[id]
             if person.get_tested():
                 print(str(id) + " tested positive and quarantined")
             else:
                 print(str(id) + " tested negative")
+
+    def add_contacts(self, num_contacts, prob_contacts, num_students):
+
+        ids_dict = self.ids_dict
+        num_chosen = {}  # keep track of how many added for each students (needed?)
+        at_contacts_max = set()
+
+        for i in range(1, num_students + 1):  # for all of the students
+
+            if i not in ids_dict.keys():
+                ids_dict[i] = Person(
+                    {})  # initialize person object with empty contacts (unless they've already been initialized)
+
+            if i not in num_chosen.keys():
+                num_chosen[i] = 0
+
+            # don't choose yourself, don't add someone if they're already a contact, don't choose someone who already has max num contacts
+            contact_options = set(range(1, num_students + 1)) - {i} - set(ids_dict[i].contacts.keys()) - at_contacts_max
+            contacts_remaining = num_contacts - num_chosen[i]  # how many still left to choose
+
+            if contacts_remaining > 0:
+
+                if len(
+                        contact_options) < contacts_remaining:  # math doesn't work out, but still need to add more contacts!
+                    all_contact_options = set(range(1, num_students + 1)) - {i} - set(ids_dict[i].contacts.keys())
+                    chosen_contacts = random.sample(list(all_contact_options), contacts_remaining)
+
+                else:
+                    chosen_contacts = random.sample(list(contact_options), contacts_remaining)  # choose contacts
+
+                num_chosen[i] += contacts_remaining  # increment count for i
+
+                # add contact and probability for current person
+                # also make current person the contact of all chosen contacts
+                # check if either current person or contact now have max number of contacts
+                for contact in chosen_contacts:
+                    ids_dict[i].contacts[contact] = prob_contacts
+
+                    if num_chosen[i] == num_contacts:  # at max
+                        at_contacts_max.add(i)
+
+                    if contact not in ids_dict.keys():  # need to intialize new contact as their own person
+                        ids_dict[contact] = Person({})
+
+                    if contact not in num_chosen.keys():
+                        num_chosen[contact] = 0
+
+                    ids_dict[contact].contacts[i] = prob_contacts  # so that contacts are mutual
+                    num_chosen[contact] += 1  # increment count for contact
+
+                    if num_chosen[i] == num_contacts:  # at max
+                        at_contacts_max.add(contact)
--- Person.py
+++ Person.py
@@ -1,15 +1,10 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import scipy
-import random
-from random import choice
 import numpy as np
 import math as math
 
 class Person:
 
     def __init__(self, contacts):
-        self.contacts = contacts # this is a 2D array [friend, weight]
+        self.contacts = contacts # this is a dictionary with friends as keys
 
         # how many days before a person is symptomatic:
         # this encodes their state (-1 = healthy, >= 0 = asymptomatic, -2 = quarantined)
@@ -17,15 +12,11 @@ class Person:
 
         self.patient_zero = False # can take this out later
 
-    def get_covid(self):
+    def get_covid(self, mean_symptomatic, standard_dev_symptomatic):
 
-        # people constants (move these out?)
-        MEAN_SYMPTOMATIC = 6 # 6 days on average
-        STANDARD_DEV_SYMPTOMATIC = 1.2 # can change this later
-
-        # randomly sample from disrubtion to get # time steps before symptomatic
+        # randomly sample from distribution to get # time steps before symptomatic
         if (self.state == -1):
-            self.state = math.floor(np.random.normal(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC))
+            self.state = math.floor(np.random.normal(mean_symptomatic, standard_dev_symptomatic))
 
     # returns true if self has it
     def get_tested(self):
