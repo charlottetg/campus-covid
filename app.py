ab19dfe03f1395abae5dbfade67168f20fabd8c0
Anna Spiro <annaspiro@annas-mbp-9.middlebury.edu>
2020-11-11 12:34:30 -0500

    changed app to be same as main

--- app.py
+++ app.py
@@ -46,44 +46,56 @@ def graph_from_data(students, close, tang, prob_close, prob_tang, mean_symptomat
 
                     if contact_id not in social_ids_dict[
                         current_id].contacts.keys():  # not already a contact (repeats?)
-                        social_ids_dict[current_id].contacts[contact_id] = prob_close  # add contact
+                        social_ids_dict[current_id].contacts[contact_id] = PROB_CLOSE  # add contact
 
                     if contact_id not in social_ids_dict.keys():  # initialize
                         social_ids_dict[contact_id] = Person({})
 
                     if current_id not in social_ids_dict[contact_id].contacts.keys():
-                        social_ids_dict[contact_id].contacts[current_id] = prob_close
+                        social_ids_dict[contact_id].contacts[current_id] = PROB_CLOSE
 
-    print(len(social_ids_dict))
     social_graph = Graph(social_ids_dict)
-    social_graph.ids_dict[1].get_covid(mean_symptomatic, standard_dev_symptomatic)  # give one person covid
+    social_graph.ids_dict[1].get_covid(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)  # give one person covid
     social_graph.ids_dict[1].patient_zero = True
 
+    print(len(social_graph.ids_dict))
+
+    # originally in show_graph (to keep pos consistent)
+    social_pos = nx.spring_layout(social_graph.networkx_graph())
+    social_graph.show_graph(PROB_CLOSE, PROB_TANG, social_graph.networkx_graph(), social_pos)
+
+
+    """
     # create graph with random close + tang contacts
     random_graph = Graph({})
-    random_graph.add_contacts(close, prob_close, students)
-    random_graph.add_contacts(tang, prob_tang, students)
-    random_graph.ids_dict[1].get_covid(mean_symptomatic, standard_dev_symptomatic)  # give one person covid
+    random_graph.add_contacts(NUM_CLOSE, PROB_CLOSE, NUM_STUDENTS)
+    random_graph.add_contacts(NUM_TANG, PROB_TANG, NUM_STUDENTS)
+    random_graph.ids_dict[1].get_covid(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)  # give one person covid
     random_graph.ids_dict[1].patient_zero = True
+
+    # originally in show_graph (to keep pos consistent)
+    random_pos = nx.spring_layout(random_graph.networkx_graph())
+    random_graph.show_graph(PROB_CLOSE, PROB_TANG, random_graph.networkx_graph(), random_pos)
+    """
 
     # one week
     for i in range(7):
-        # random_graph.graph_spread(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)
-        social_graph.graph_spread(mean_symptomatic, standard_dev_symptomatic)
+        #random_graph.graph_spread(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)
+        social_graph.graph_spread(MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC)
 
-        people_to_test = random.sample(list(range(1, students)), round(students / 21))
+        people_to_test = random.sample(list(range(1, NUM_STUDENTS)), round(NUM_STUDENTS / 21))
 
-        # random_graph.dynamic_test(people_to_test)
+        #random_graph.dynamic_test(people_to_test)
         social_graph.dynamic_test(people_to_test)
 
-    # random_graph.show_graph(PROB_CLOSE, PROB_TANG)
-    # random_graph.print_stats()
-    # random_graph.print_contacts_info()
 
-    #social_graph.show_graph(prob_close, prob_tang) #we don't want to use nx in the back end
+    #random_graph.show_graph(PROB_CLOSE, PROB_TANG, random_graph.networkx_graph(), random_pos)
+    #random_graph.print_stats()
+    #random_graph.print_contacts_info()
+
+    social_graph.show_graph(PROB_CLOSE, PROB_TANG, social_graph.networkx_graph(), social_pos) #we don't want to use nx in the back end
     social_graph.print_stats()
-    # social_graph.print_contacts_info()
-
+    #social_graph.print_contacts_info()
 
 
 
