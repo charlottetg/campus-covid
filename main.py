393e8cde381767e6d613f5536eb668372ee3e8f1
Anna Spiro <annaspiro@Annas-MBP-9.home>
2020-11-28 13:35:34 -0500

    testing

--- main.py
+++ main.py
@@ -93,6 +93,7 @@ def run_simulation(graph, num_runs, days, fraction_tested_per_day, mean_symptoma
             quarantined[j] += current_run_graph.num_quarantined()
 
             if i == 0: # only save first run graphs
+                current_run_graph.print_stats()
                 current_day_graph = deepcopy(current_run_graph).networkx_graph() # want as a networkx graph
                 graphs.append(current_day_graph) # kind of weird, but works!
 
@@ -108,7 +109,7 @@ def run_simulation(graph, num_runs, days, fraction_tested_per_day, mean_symptoma
     return graphs, single_stats, average_stats
 
 
-results = run_simulation(random_graph, 2, 3, 20, MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC, False)
+results = run_simulation(random_graph, 2, 3, 21, MEAN_SYMPTOMATIC, STANDARD_DEV_SYMPTOMATIC, False)
 graphs = results[0]
 single_stats = results[1]
 average_stats = results[2]
