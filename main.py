ca17c0ac37c4c0327273891016e136f4242f8ed9
Anna Spiro <annaspiro@Annas-MBP-9.home>
2020-11-28 13:25:17 -0500

    made run simulation return array of networkx graphs

--- main.py
+++ main.py
@@ -67,11 +67,11 @@ random_pos = nx.spring_layout(random_graph.networkx_graph())
 def run_simulation(graph, num_runs, days, fraction_tested_per_day, mean_symptomatic, standard_dev_symptomatic, all_contacts):
     """
     Runs a simulated spread, returns [graphs, single_stats, average_stats]
-    graphs is array of graphs for each day of the simulation *for the first run*
+    graphs is array of networkx graphs for each day of the simulation *for the first run*
     single_stats is a 2D array of population [healthy, asymptomatic, quarantined] for each day *for the first run*
     average_stats is a 2D array of population [healthy, asymptomatic, quarantined] for each day *on average*
 
-    note: all contacts is a Boolean
+    note: all_contacts is a Boolean
     """
 
     healthy = [0] * days
@@ -93,7 +93,7 @@ def run_simulation(graph, num_runs, days, fraction_tested_per_day, mean_symptoma
             quarantined[j] += current_run_graph.num_quarantined()
 
             if i == 0: # only save first run graphs
-                current_day_graph = deepcopy(current_run_graph)
+                current_day_graph = deepcopy(current_run_graph).networkx_graph() # want as a networkx graph
                 graphs.append(current_day_graph) # kind of weird, but works!
 
                 if j == days - 1: # last day of first run, save single_stats
@@ -113,8 +113,10 @@ graphs = results[0]
 single_stats = results[1]
 average_stats = results[2]
 
+"""
 for graph in graphs:
     graph.print_stats()
+"""
 
 print("single stats = " + str(single_stats))
 print("average stats = " + str(average_stats))
