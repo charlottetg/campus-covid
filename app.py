ae15c5d9e6ee6b0a7239c7b986881e84cf0a6425
charlotte gray <charlottetgray@gmail.com>
2020-11-28 13:34:25 -0500

    fixed the html templating

--- app.py
+++ app.py
@@ -3,12 +3,26 @@ from Person import Person
 from Graph import Graph
 import pandas as pd
 import random
+from pyvis.network import Network
 app = Flask(__name__)
 
-@app.route("/")
-def testin():
-    return render_template('hello.html')
 
+@app.route("/<day>")
+def testofPyvis(day):
+    g = Network()
+    g.add_node(0, label="0")
+    for i in range(1, int(day)):
+        g.add_node(i, label=str(i))
+        g.add_edge(i, i-1)
+        #g.add_node(1, label="b")
+    #g.add_edge(0, 1)
+    strin = "templates/" + day + ".html"
+    g.show(strin)
+    return render_template(day+".html")
+    #return render_template(strin)
+
+
+"""
 @app.route("/<id>")
 def persondata(id):
     iid = int(id)
@@ -22,7 +36,7 @@ def persondata(id):
 def time(day):
     return "it is now day: " + day
 
-
+"""
 
 
 if __name__ == "__main__":
